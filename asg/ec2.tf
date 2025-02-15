b86f5dd5e016c2d37fe0e27ec6340109a371b3a4
Kanyshai-Nurdinova <79550683+Kanyshai-Nurdinova@users.noreply.github.com>
2025-02-15 05:53:01 -0500

    added ec2

---
+++ asg/ec2.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "wordpress" {
+  ami           = "data.aws_ami.image.id"  # Use the latest Amazon Linux AMI
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.ec2_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras enable php8.0
+              yum install -y httpd php php-mysqlnd
+              systemctl start httpd
+              systemctl enable httpd
+              cd /var/www/html
+              wget https://wordpress.org/latest.tar.gz
+              tar -xzf latest.tar.gz
+              cp -r wordpress/* .
+              rm -rf wordpress latest.tar.gz
+              chown -R apache:apache /var/www/html
+              chmod -R 755 /var/www/html
+              systemctl restart httpd
+              EOF
+}
--- asg/userdata.sh
+++ asg/userdata.sh
@@ -2,3 +2,8 @@
 sudo apt-get install apache2 -y
 sudo systemctl start apache2 
 sudo systemctl enable apache2 
+
+sudo yum update -y
+sudo yum install -y httpd php php-mysqlnd
+sudo systemctl start httpd
+sudo systemctl enable httpd
