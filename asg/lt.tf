f49ce0e288aadb428a4b91483e83351763fa5531
Kanyshai-Nurdinova <79550683+Kanyshai-Nurdinova@users.noreply.github.com>
2025-02-17 10:54:02 -0500

    added userdata

--- asg/lt.tf
+++ asg/lt.tf
@@ -4,5 +4,20 @@ resource "aws_launch_template" "example" {
   instance_type        = "c5.large"
   security_group_names = [aws_security_group.ec2_sg.name]
   key_name             = aws_key_pair.asg-key-pair.key_name
-  user_data            = filebase64("${path.module}/userdata.sh")
+ user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras enable php8.0
+              yum install -y httpd php php-mysqlnd
+              systemctl start httpd
+              systemctl enable httpd
+              cd /var/www/html
+              wget https://wordpress.org/latest.tar.gz
+              tar -xzf latest.tar.gz
+              cp -r wordpress/* .
+              rm -rf wordpress latest.tar.gz
+              chown -R apache:apache /var/www/html
+              chmod -R 755 /var/www/html
+              systemctl restart httpd
+              EOF
 }
