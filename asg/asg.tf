14094f2bb959c9f4176ecd3d232120a4eba317d4
Kanyshai-Nurdinova <79550683+Kanyshai-Nurdinova@users.noreply.github.com>
2025-02-17 11:27:48 -0500

    changed lt

--- asg/asg.tf
+++ asg/asg.tf
@@ -1,25 +1,48 @@
-resource "aws_autoscaling_group" "example" {
+# resource "aws_autoscaling_group" "example" {
+#   availability_zones = data.aws_availability_zones.all.names
+#   desired_capacity   = 3
+#   max_size           = 99
+#   min_size           = 3
+#   mixed_instances_policy {
+#     launch_template {
+#       launch_template_specification {
+#         launch_template_id = aws_launch_template.example.id
+#       }
+#       override {
+#         instance_type     = "c4.large"
+#         weighted_capacity = "1"
+#       }
+#       override {
+#         instance_type     = "c3.large"
+#         weighted_capacity = "2"
+#       }
+#       override {
+#         instance_type     = "m5.large"
+#         weighted_capacity = "3"
+#       }
+#     }
+#   }
+# }
+
+resource "aws_autoscaling_group" "wordpress_asg" {
   availability_zones = data.aws_availability_zones.all.names
-  desired_capacity   = 3
-  max_size           = 99
-  min_size           = 3
-  mixed_instances_policy {
-    launch_template {
-      launch_template_specification {
-        launch_template_id = aws_launch_template.example.id
-      }
-      override {
-        instance_type     = "c4.large"
-        weighted_capacity = "1"
-      }
-      override {
-        instance_type     = "c3.large"
-        weighted_capacity = "2"
-      }
-      override {
-        instance_type     = "m5.large"
-        weighted_capacity = "3"
-      }
-    }
-  }
+  desired_capacity    = 2
+  min_size           = 1
+  max_size           = 3
+  launch_configuration = aws_launch_configuration.wordpress_lc.id
+}
+
+resource "aws_launch_configuration" "wordpress_lc" {
+  name          = "wordpress-lc"
+  image_id      = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.ec2_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd php php-mysqlnd
+              systemctl start httpd
+              systemctl enable httpd
+              EOF
 }
--- asg/lt.tf
+++ asg/lt.tf
@@ -1,23 +1,23 @@
-resource "aws_launch_template" "example" {
-  name_prefix          = "example"
-  image_id             = "ami-0c55b159cbfafe1f0"
-  instance_type        = "c5.large"
-  security_group_names = [aws_security_group.ec2_sg.name]
-  key_name             = aws_key_pair.asg-key-pair.key_name
- user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              amazon-linux-extras enable php8.0
-              yum install -y httpd php php-mysqlnd
-              systemctl start httpd
-              systemctl enable httpd
-              cd /var/www/html
-              wget https://wordpress.org/latest.tar.gz
-              tar -xzf latest.tar.gz
-              cp -r wordpress/* .
-              rm -rf wordpress latest.tar.gz
-              chown -R apache:apache /var/www/html
-              chmod -R 755 /var/www/html
-              systemctl restart httpd
-              EOF
-}
+# resource "aws_launch_template" "example" {
+#   name_prefix          = "example"
+#   image_id             = "ami-0c55b159cbfafe1f0"
+#   instance_type        = "c5.large"
+#   security_group_names = [aws_security_group.ec2_sg.name]
+#   key_name             = aws_key_pair.asg-key-pair.key_name
+#  user_data = <<-EOF
+#               #!/bin/bash
+#               yum update -y
+#               amazon-linux-extras enable php8.0
+#               yum install -y httpd php php-mysqlnd
+#               systemctl start httpd
+#               systemctl enable httpd
+#               cd /var/www/html
+#               wget https://wordpress.org/latest.tar.gz
+#               tar -xzf latest.tar.gz
+#               cp -r wordpress/* .
+#               rm -rf wordpress latest.tar.gz
+#               chown -R apache:apache /var/www/html
+#               chmod -R 755 /var/www/html
+#               systemctl restart httpd
+#               EOF
+# }
