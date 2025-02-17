798af94355f5ecc9a2c8a78ba293efdd45af4a11
Kanyshai-Nurdinova <79550683+Kanyshai-Nurdinova@users.noreply.github.com>
2025-02-17 02:32:45 -0500

    changed ami

--- asg/data.tf
+++ asg/data.tf
@@ -14,13 +14,10 @@ data "aws_ami" "image" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  owners = ["099720109477"] # Canonical
+ 
+  owners = ["amazon"] # Canonical
 }
 
 output "AMI_ID" {
