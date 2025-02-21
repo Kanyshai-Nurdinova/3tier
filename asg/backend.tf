7f8800e61d7948ec0f1d91ce2579ef4340c4b8f1
Kanyshai-Nurdinova <79550683+Kanyshai-Nurdinova@users.noreply.github.com>
2025-02-21 18:24:43 -0500

    =

--- asg/backend.tf
+++ asg/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "3tier-app-oct24"
+    bucket         = "3tier-app-oct2024"
     key            = "terraform/statefile.tfstate"
     region         = "us-east-2"
     encrypt        = true
